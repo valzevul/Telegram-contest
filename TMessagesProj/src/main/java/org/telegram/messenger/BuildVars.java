1de95485c736d325f33dc8c7e0d01fd476ecb300
Vadim <vadim@drobinin.com>
2025-07-03 00:17:07 +0100

    Rename package, etc

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -74,7 +74,7 @@ public class BuildVars {
     private static Boolean betaApp;
     public static boolean isBetaApp() {
         if (betaApp == null) {
-            betaApp = ApplicationLoader.applicationContext != null && "org.telegram.messenger.beta".equals(ApplicationLoader.applicationContext.getPackageName());
+            betaApp = ApplicationLoader.applicationContext != null && "com.drobinin.telegram.beta".equals(ApplicationLoader.applicationContext.getPackageName());
         }
         return betaApp;
     }
